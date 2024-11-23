0328b53b64cbef72544335c71b4113d477d12447
Andy <andyclos@student.usm.my>
2024-11-24 02:44:47 +0800

    setup login page

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.login_view, name='login'),
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
     path('dashboard/', views.dashboard_view, name='dashboard'),
