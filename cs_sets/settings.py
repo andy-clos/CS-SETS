204cb99a937351c8815aaeeb12a8bf3139e2115b
Andy <andyclos@student.usm.my>
2025-01-10 00:31:14 +0800

    initialize table data to 0 when no data

--- cs_sets/settings.py
+++ cs_sets/settings.py
@@ -72,6 +72,11 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            
+            'libraries':{
+            'custom_filters': 'app.custom_filters',
+            
+            }
         },
     },
 ]
