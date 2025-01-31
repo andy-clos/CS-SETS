bf8ed6859d8811b743e088defe425998d2ffe1bb
Andyclos Boon Mee <74916169+andy-clos@users.noreply.github.com>
2025-01-31 22:58:37 +0800

    Update settings.py

--- cs_sets/settings.py
+++ cs_sets/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-6jcjl#g1ybdf+_u%eu43mc504)+5s1q)8q-12r$t%_laqqmg@u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
